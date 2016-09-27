277ebeb487e13339feff1924caf1084c429f95c3
Karol Kucharski <187746@edu.p.lodz.pl>
2016-09-27 12:22:47 +0200

    Implamentacja callbacka.

---
+++ src/main/java/lab1/lab1/CallbackImpl.java
@@ -0,0 +1,22 @@
+package lab1.lab1;
+import java.util.*;
+
+public class CallbackImpl implements Callback {
+	
+	private Scanner scan;
+
+	public String callback(boolean result) {
+		if(result)
+			return null;
+		while(true) {
+			System.out.print("Czy chcesz zapisac plik pod inna nazwa? [T/N] ");
+			String c = scan.nextLine();
+			if(c.startsWith("T") || c.startsWith("t")) {
+				String newFileName = scan.nextLine();
+				return newFileName;
+			} else if(c.startsWith("N") || c.startsWith("n"))
+				return null;
+		}
+	}
+
+}
