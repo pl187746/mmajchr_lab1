97338497f3c6044f0e2fd150a0a933c1f6c66816
Karol Kucharski <187746@edu.p.lodz.pl>
2016-09-27 12:28:41 +0200

    Wywolanie funkcji zapisujacej plik

--- src/main/java/lab1/lab1/App.java
+++ src/main/java/lab1/lab1/App.java
@@ -13,5 +13,7 @@ public class App
         String strPopr = scan.nextLine();
         System.out.print("Podaj string niepoprawny: ");
         String strNiePopr = scan.nextLine();
+        CallbackImpl callback = new CallbackImpl(scan);
+        FileOperation.saveFile(fileName, strPopr, strNiePopr, callback);
     }
 }
