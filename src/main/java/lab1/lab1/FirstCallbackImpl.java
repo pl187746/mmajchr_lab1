d9040a493c5352c4afc6c329606e2189a6db0156
Karol Kucharski <187746@edu.p.lodz.pl>
2016-10-11 12:06:51 +0200

    Zmiana implementacji pierwszego callbacka

--- src/main/java/lab1/lab1/FirstCallbackImpl.java
+++ src/main/java/lab1/lab1/FirstCallbackImpl.java
@@ -9,13 +9,18 @@ public class FirstCallbackImpl implements FirstCallback {
 		this.scan = sc;
 	}
 
-	public String callback(boolean result) {
-		if(result)
+	public String callback(SecondCallback callback) {
+		SaveResult result = callback.callback();
+		if(result == null || result.isSuccess())
 			return null;
 		while(true) {
 			System.out.print("Czy chcesz zapisac plik pod inna nazwa? [T/N] ");
 			String c = scan.nextLine();
 			if(c.startsWith("T") || c.startsWith("t")) {
+				System.out.println("Poprzednie ustawienia: ");
+				System.out.println("Nazwa pliku: " + result.getFileName());
+				System.out.println("String powodzenia: " + result.getCorrectMsg());
+				System.out.println("String niepowodzenia: " + result.getIncorrectMsg());
 				System.out.print("Nowa nazwa pliku: ");
 				String newFileName = scan.nextLine();
 				return newFileName;
